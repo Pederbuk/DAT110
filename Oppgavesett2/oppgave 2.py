fc33a739b29e766e713b7e7b81fab1845beea583
Peder <peder.ormen.bukaasen@nmbu.no>
2022-02-07 19:40:44 +0100

    ferdig med oppgave 2c

---
+++ Oppgavesett2/oppgave 2.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#list to store the data
+snowfall_data = []
+
+#opens the file and reads the lines it needs
+with open('snowfall.csv') as infile:
+    infile.readline()#removes the header
+    for line in infile.readlines():
+        temp_line = line.strip().split(',')
+        if temp_line[2] == 'NA':
+            new_line = 0
+        else:
+            new_line = float(temp_line[2])
+        snowfall_data.append(new_line)
+print(snowfall_data)
+
+plt.boxplot(snowfall_data)
+plt.title('Snowfall')
+plt.ylabel('Snømengde i tommer')
+plt.show()
