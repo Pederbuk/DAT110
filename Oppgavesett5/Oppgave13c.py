1a211059948a926c10fa22c7ed4f34ad82c4b0bd
PederBuk <peder.ormen.bukaasen.email@nmbu.no>
2022-02-28 21:46:11 +0100

    oppgave 1.3c ferdig

---
+++ Oppgavesett5/Oppgave13c.py
@@ -0,0 +1,30 @@
+import random as rd
+import matplotlib.pyplot as plt
+
+rd.seed(1235)
+
+# leser inn data fra filen og lagrer de som en liste
+data = []
+with open('Antall_drinker.txt') as infile:
+     for line in infile:
+         line = float(line)
+         data.append(line)
+
+
+# ta 100 ulike sampler og finne middelverdi for disse
+avreage_list = []
+for i in range(100):
+    sample = rd.sample(data,10)
+    avreage = sum(sample)/len(sample)
+    avreage_list.append(avreage)
+
+
+simulated_avg = sum(avreage_list)/len(avreage_list)
+plt.title('Antall drinker 100 utvalg på 10 pax')
+plt.hist(avreage_list,label=f'gjenomsnitt: {simulated_avg:.3f}')
+plt.legend()
+plt.show()
+
+
+
+
