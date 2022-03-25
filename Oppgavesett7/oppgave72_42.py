ca1d113cbfa8b63a116696cad931bc814dc64b16
PederBuk <peder.ormen.bukaasen.email@nmbu.no>
2022-03-25 12:29:09 +0100

    fikset feil i koden

--- Oppgavesett7/oppgave72_42.py
+++ Oppgavesett7/oppgave72_42.py
@@ -1,29 +1,12 @@
 import pandas as pd
 import scipy.stats as stats
+import matplotlib.pyplot as plt
 df = pd.read_csv('footballplayers.csv',sep=';')
-teams = ['DallasCowboys', 'GreenBayPackers','DenverBroncos','MiamiDolphins','SanFransiscoFortyNiners']
-teams_mean = df.mean()
-teams_sum = df.count()
-teams_std = df.std()
-n = df.stack().count()
-total_mean = df.stack().mean()
 
-sst = 0
-ssg = 0
-for team in teams:
-    for value in df[team]:
-        sst += (value - total_mean)**2
-    ssg += n*(teams_mean[team] - total_mean)**2
-sse = sst - ssg
-
-dft = n-1
-dfg = 5
-dfe = dft-dfg
-
-
-msg = ssg/dfg
-mse = sse/dfe
-
-F = msg/mse
-p = stats.f.sf(F,dfg,dft)
+p = stats.f_oneway(df['DallasCowboys'],df['GreenBayPackers'],df['DenverBroncos'],df['MiamiDolphins'],df['SanFransiscoFortyNiners'])
 print(p)
+
+#betingelsene for anova?
+
+fig1 = plt.figure()
+fig1.subplot(1.)
--- Oppgavesett7/oppgave72_42b.py
+++ Oppgavesett7/oppgave72_42b.py
@@ -1,30 +1,13 @@
 import pandas as pd
 import scipy.stats as stats
+from statsmodels.stats.multicomp import (pairwise_tukeyhsd,MultiComparison)
 df = pd.read_csv('startupcosts.csv',sep=';')
-types_resturant = ['pizzarestaurant','bakery','shoestore','giftshop','petstore']
-types_rest_mean = df.mean()
-types_rest_sum = df.count()
-types_rest_std = df.std()
-n = df.stack().count()
-total_mean = df.stack().mean()
 
-sst = 0
-ssg = 0
-for rest in types_resturant:
-    for value in df[rest]:
-        sst += (value - total_mean) ** 2
-
-    ssg += n * (types_rest_mean[rest] - total_mean) ** 2
-sse = sst - ssg
-
-dft = n-1
-dfg = 5
-dfe = dft-dfg
-
-
-msg = ssg/dfg
-mse = sse/dfe
-
-F = msg/mse
-p = stats.f.sf(F,dfg,dft)
+p = stats.f_oneway(df['pizzarestaurant'],df['bakery'],df['shoestore'],df['giftshop'],df['petstore'])
 print(p)
+
+data_for_mc = df.stack().reset_index()
+data_for_mc = data_for_mc.rename(columns={'level_o':id,'level_1':'Type_res', 0:'startkostnad'})
+
+multiple_sammenlikninger = MultiComparison(data_for_mc['startkostnad'],data_for_mc['Type_res'])
+print(multiple_sammenlikninger.tukeyhsd().summary())
